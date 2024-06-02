93cd5bc0e0b54d746b4c40607ef64575706da722
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-02 17:16:17 +0530

    altenative inefficient approach for useMemo hook by uusing useEffect hook

--- src/components/CounterWithMemoization.tsx
+++ src/components/CounterWithMemoization.tsx
@@ -10,6 +10,18 @@ export default function CounterWithMemoization() {
     console.log("memo called");
     return (parseInt(input) * (parseInt(input) + 1)) / 2;
   }, [input]);
+
+  //other approach
+  /* 
+  const [counter, setCounter] = useState(0);
+  useEffect(()=>{
+    for(int i=0; i<=parseInt(input); i++){
+      counter += i;
+    }
+    setCounter(counter);
+  },[inputValue])
+  */
+
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: 2 }}>
       <input
@@ -26,7 +38,7 @@ export default function CounterWithMemoization() {
         }}
         style={{ border: "2px solid black", background: "#EEE" }}
       >
-        Counter {count}
+        Counter {count} {/* counter for other approach write the counter variable here*/}
       </button>
       <div>Sum is {sum}</div>
     </div>
